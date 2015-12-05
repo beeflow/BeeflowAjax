c2d24328640bd5baa3bd59e0d2dd62ad79454de1
Rafal Przetakowski <rafal.p@beeflow.co.uk>
2015-12-05 16:05:58 +0100

    Tests coverage

--- Tests/Utils/BeeflowAjaxResponseTest.php
+++ Tests/Utils/BeeflowAjaxResponseTest.php
@@ -31,28 +31,32 @@ class BeeflowAjaxResponseTest extends TestCase {
 
 	private $ajaxResponse;
 
-	public function setUp() {
+	public function setUp()
+	{
 		$this->ajaxResponse = new BeeflowAjaxResponse();
 	}
 
 	/**
 	 * @test
 	 */
-	public function getJson() {
+	public function getJson()
+	{
 		$this->assertEquals('[]', $this->ajaxResponse->getJson());
 	}
 
 	/**
 	 * @test
 	 */
-	public function getArray() {
+	public function getArray()
+	{
 		$this->assertTrue(is_array($this->ajaxResponse->getArray()));
 	}
 
 	/**
 	 * @test
 	 */
-	public function alert() {
+	public function alert()
+	{
 		$this->ajaxResponse->alert('Test message');
 		$this->assertEquals('[{"cmd":"alert","data":"Test message"}]', $this->ajaxResponse->getJson());
 	}
@@ -60,7 +64,8 @@ class BeeflowAjaxResponseTest extends TestCase {
 	/**
 	 * @test
 	 */
-	public function debug() {
+	public function debug()
+	{
 		$this->ajaxResponse->debug('Test message');
 		$this->assertEquals('[{"cmd":"debug","data":"Test message"}]', $this->ajaxResponse->getJson());
 	}
@@ -68,7 +73,8 @@ class BeeflowAjaxResponseTest extends TestCase {
 	/**
 	 * @test
 	 */
-	public function append() {
+	public function append()
+	{
 		$this->ajaxResponse->append('element-id', 'Test');
 		$this->assertEquals('[{"cmd":"append","id":"element-id","data":"Test"}]', $this->ajaxResponse->getJson());
 	}
@@ -76,7 +82,8 @@ class BeeflowAjaxResponseTest extends TestCase {
 	/**
 	 * @test
 	 */
-	public function assign() {
+	public function assign()
+	{
 		$this->ajaxResponse->assign('element-id', 'Test');
 		$this->assertEquals('[{"cmd":"assign","id":"element-id","data":"Test"}]', $this->ajaxResponse->getJson());
 	}
@@ -84,7 +91,8 @@ class BeeflowAjaxResponseTest extends TestCase {
 	/**
 	 * @test
 	 */
-	public function redirect() {
+	public function redirect()
+	{
 		$this->ajaxResponse->redirect('example.com');
 		$this->assertEquals('[{"cmd":"redirect","url":"example.com","data":null}]', $this->ajaxResponse->getJson());
 	}
@@ -92,7 +100,8 @@ class BeeflowAjaxResponseTest extends TestCase {
 	/**
 	 * @test
 	 */
-	public function reloadLocation() {
+	public function reloadLocation()
+	{
 		$this->ajaxResponse->reloadLocation();
 		$this->assertEquals('[{"cmd":"reloadLocation","data":null}]', $this->ajaxResponse->getJson());
 	}
@@ -100,7 +109,8 @@ class BeeflowAjaxResponseTest extends TestCase {
 	/**
 	 * @test
 	 */
-	public function remove() {
+	public function remove()
+	{
 		$this->ajaxResponse->remove('element-id');
 		$this->assertEquals('[{"cmd":"remove","id":"element-id","data":null}]', $this->ajaxResponse->getJson());
 	}
@@ -108,7 +118,8 @@ class BeeflowAjaxResponseTest extends TestCase {
 	/**
 	 * @test
 	 */
-	public function addClass() {
+	public function addClass()
+	{
 		$this->ajaxResponse->addClass('element-id', 'new-classname');
 		$this->assertEquals('[{"cmd":"addClass","id":"element-id","data":"new-classname"}]', $this->ajaxResponse->getJson());
 	}
@@ -116,7 +127,8 @@ class BeeflowAjaxResponseTest extends TestCase {
 	/**
 	 * @test
 	 */
-	public function removeClass() {
+	public function removeClass()
+	{
 		$this->ajaxResponse->removeClass('element-id', 'new-classname');
 		$this->assertEquals('[{"cmd":"removeClass","id":"element-id","data":"new-classname"}]', $this->ajaxResponse->getJson());
 	}
@@ -124,7 +136,8 @@ class BeeflowAjaxResponseTest extends TestCase {
 	/**
 	 * @test
 	 */
-	public function removeAllClasses() {
+	public function removeAllClasses()
+	{
 		$this->ajaxResponse->removeClass('element-id');
 		$this->assertEquals('[{"cmd":"removeClass","id":"element-id","data":null}]', $this->ajaxResponse->getJson());
 	}
@@ -132,7 +145,39 @@ class BeeflowAjaxResponseTest extends TestCase {
 	/**
 	 * @test
 	 */
-	public function returnJson() {
+	public function show()
+	{
+		$this->ajaxResponse->show('element-id');
+		$this->assertEquals('[{"cmd":"show","id":"element-id","data":null}]', $this->ajaxResponse->getJson());
+	}
+
+	/**
+	 * @test
+	 */
+	public function hide()
+	{
+		$this->ajaxResponse->hide('element-id');
+		$this->assertEquals('[{"cmd":"hide","id":"element-id","data":null}]', $this->ajaxResponse->getJson());
+	}
+
+	/**
+	 * @test
+	 */
+	public function printOutput()
+	{
+		$this->ajaxResponse->hide('element-id');
+		ob_start();
+		$this->ajaxResponse->printOutput();
+		$given = ob_get_contents();
+		ob_end_clean();
+		$this->assertEquals('[{"cmd":"hide","id":"element-id","data":null}]', $given);
+	}
+	
+	/**
+	 * @test
+	 */
+	public function returnJson()
+	{
 		$this->ajaxResponse->returnJson(array('test-field' => 'test value'));
 		$this->assertEquals('{"test-field":"test value"}', $this->ajaxResponse->getJson());
 	}
@@ -140,7 +185,17 @@ class BeeflowAjaxResponseTest extends TestCase {
 	/**
 	 * @test
 	 */
-	public function script() {
+	public function returnJsonWithErrors()
+	{
+		$this->ajaxResponse->returnJson(array('errors' => array('Some error message', 'Other error message')));
+		$this->assertEquals('["Some error message","Other error message"]', $this->ajaxResponse->getJson());
+	}
+
+	/**
+	 * @test
+	 */
+	public function script()
+	{
 		$this->ajaxResponse->script('alert()');
 		$this->assertEquals('[{"cmd":"runScript","data":"alert()"}]', $this->ajaxResponse->getJson());
 	}
@@ -148,7 +203,8 @@ class BeeflowAjaxResponseTest extends TestCase {
 	/**
 	 * @test
 	 */
-	public function toString() {
+	public function toString()
+	{
 		$this->assertEquals('[]', $this->ajaxResponse);
 	}
 
